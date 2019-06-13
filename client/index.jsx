d9ca52956913914dec319e51e0dc1a82b53ef1e3
ljw7975 <leejw7975@gmail.com>
2019-06-13 18:43:01 -0400

    Fix signup functionality, add vscode into gitignore

--- client/index.jsx
+++ client/index.jsx
@@ -30,8 +30,10 @@ class App extends React.Component {
         )
     }
 }
+
 ReactDom.render(
     <App />,
     document.getElementById('app')
 )
+
 export default App;
--- routes/login.js
+++ routes/login.js
@@ -4,18 +4,7 @@ const user = require('../db/user');
 
 const router = express.Router();
 var urlencodedParser = bodyParser.urlencoded({extended: false});
-/*
-//Sign Up
-router.post('/signup', urlencodedParser, function(req, res){
-    console.log(req.body);
-    var newUser = new user.User(req.body);
-    newUser.save().then(item => {
-        res.send('signup is successful')
-    }).catch(err => {
-        res.status(400).send(err, "Unable to save to database");
-    });
-});
-*/
+
 router.post('/signup', urlencodedParser, async(req, res) => {
     console.log(req.body);
     let userid = await user.User.findOne({id: req.body.id});
@@ -24,14 +13,12 @@ router.post('/signup', urlencodedParser, async(req, res) => {
          newUser.save().then(item => {
         res.send('signup is successful')
         })
-    } 
-    else {
+    } else {
         res.status(400).send("Unable to save to database");
     }
     
 });
 
-//Sign in logic
 //Sign in logic
 router.post('/signin', urlencodedParser, async(req, res) => {
    console.log(req.body)
