957c583d304129108e5af146aa8bd6ab7d24bf00
ljw7975 <leejw7975@gmail.com>
2019-06-20 19:54:50 -0400

    Split the page into two sections and display name on the right side upon clicking celebrity name on the left side

--- client/components/findStar.jsx
+++ client/components/findStar.jsx
@@ -4,29 +4,44 @@ class FindStar extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            Celebrity: []
+            Celebrity: [],
+            click: ''
         };
+        this.handleClick = this.handleClick.bind(this);
     }
     
-componentDidMount(){
-    fetch('http://localhost:5000/list')
-    .then((res) => res.json())
-    .then((data) => {
-        console.log(data);
-        this.setState({Celebrity:data}, () => console.log(this.state.Celebrity));
+    componentDidMount(){
+        fetch('http://localhost:5000/list')
+        .then((res) => res.json())
+        .then((data) => {
+            this.setState({Celebrity:data}, () => console.log(this.state.Celebrity));
         }
     )}
+
+    handleClick(event){
+        this.setState({click: event.target.id}, () => console.log(this.state.click));
+        event.preventDefault();
+    }
     
     render() {
         return (
             <div>
-                <ul>
-                    {this.state.Celebrity.map(celeb => 
-                        <div key = {celeb._id}>
-                            {celeb.name}
-                        </div>
-                    )}
-                </ul>
+                <div className = "row">
+                    <div className = "column">
+                        <ul>
+                            {this.state.Celebrity.map(celeb => 
+                                <div key = {celeb._id}>
+                                    <a href="#" onClick={this.handleClick} id = {celeb.name}>
+                                        {celeb.name}
+                                    </a>
+                                </div>
+                            )}
+                        </ul>
+                    </div>
+                    <div className = "column">
+                        {this.state.click}
+                    </div>
+                </div>
             </div>
         
         );
