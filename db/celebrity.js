efc04132cb0d34c8a45a3726a16710cad042543b
ljw7975 <leejw7975@gmail.com>
2019-07-17 18:51:29 -0400

    Change list of celebrity names into schema and insert to the database

--- db/celebrity.js
+++ db/celebrity.js
@@ -21,20 +21,7 @@ var Celebrity = mongoose.model('Celebrity', CelebritySchema, 'Celebrity');
 module.exports.Celebrity = Celebrity;
 
 module.exports.init = function(){
-    var celebs = [{name: 'BTS', gender: 'M', imgPath: 
-                    ['client/src/BTS1.jpg', 'client/src/BTS2.jpeg', 'client/src/BTS3.jpg', 'client/src/BTS4.jpg']},
-                  {name: 'IZ*ONE', gender: 'F',imgPath: 
-                    ['client/src/IZ*ONE1.jpg', 'client/src/IZ*ONE2.jpg', 'client/src/IZ*ONE3.jpeg']},
-                  {name: 'TWICE', gender: 'F', imgPath: ['client/src/TWICE1.jpg']}]
 
-    Celebrity.collection.insert(celebs, function (err, docs) {
-        if (err) { 
-            console.error('celebs are already in the database');
-        } else {
-            console.log("Celebs are inserted to collection");
-        }
-    });  
-    
     var crawler = new Crawler({
         maxConnections : 10,
         
@@ -44,16 +31,32 @@ module.exports.init = function(){
             } else {
                 var $ = res.$;
                 var list = $("div.div-col> ul> li>[href *= 'wiki']a").toArray();
-                var stars =[]
+                var celebName = [];
+                var stars = [];
+
+                // Get a list of celebrity names
                 list.map(celeb => {
-                    stars.push(celeb.attribs.title);
+                    celebName.push(celeb.attribs.title);
                 })
-                console.log(stars);
-            }
+
+                // Transfrom names into Celebrity schema and store it into stars 
+                celebName.map(star => {
+                    var celebSchema = {name: `${star}`};
+                    stars.push(celebSchema)
+                })
+
+                //Insert to the database
+                Celebrity.collection.insert(stars, function (err, docs) {
+                    if (err) { 
+                        console.error('celebs are already in the database');
+                    } else {
+                        console.log("Celebs are inserted to collection");
+                    }
+                });
+            }   
             done();
         }
     });
 
     crawler.queue('https://en.wikipedia.org/wiki/List_of_South_Korean_idol_groups_(2010s)');
-
 }
