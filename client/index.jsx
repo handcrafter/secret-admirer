5ff46248689f2a7f5b332cb02085025448f39b7b
ljw7975 <leejw7975@gmail.com>
2019-07-18 19:21:25 -0400

    front page with search box

--- client/index.jsx
+++ client/index.jsx
@@ -8,24 +8,14 @@ require('./css/index.css');
 class App extends React.Component {
     render() {
         return(
-                <div>
-                    <div className = "row">
-                        <div className = "column">
-                            <div className = "container">
-                                <img className = "imgBlur"
-                                    src = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/‘LG_Q7_BTS_에디션’_예약_판매_시작_%2842773472410%29_%28cropped%29.jpg/560px-‘LG_Q7_BTS_에디션’_예약_판매_시작_%2842773472410%29_%28cropped%29.jpg" 
-                                    alt = "main image"
-                                    width = "100%"
-                                    height = "100%"/>
-                                <div className ="centered">
-                                    <h1 color="white"> Search up images of your favourite celebrities! </h1>
-                                </div>
-                            </div>
-                        </div>
-                        <div className = "column mainRight">
-                            <Signin />
-                        </div>
-                    </div>
+                <div className="frontPage">
+                    <form>
+                        <p className = "titleColour">SECRET ADMIRER</p>
+                        <br/>
+                        <fieldset>
+                            <input type="text" placeholder="Search" className="frontSearch"/>
+                        </fieldset>
+                    </form>
                 </div>
         )
     }
