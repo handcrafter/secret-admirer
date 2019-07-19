57351310c3f4a5952c58a1003e9c2c513fc923a1
ljw7975 <leejw7975@gmail.com>
2019-07-19 00:26:04 -0400

    minor naming fix

--- client/index.jsx
+++ client/index.jsx
@@ -1,15 +1,14 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import Signin from './signin.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Next from './components/searchImage.jsx';
+import SearchImage from './components/searchImage.jsx';
 
 require('./css/index.css');
 
 class App extends React.Component {
 
     handleSignup(event) {
-        ReactDOM.render(<Next />, document.getElementById("app"));
+        ReactDOM.render(<SearchImage />, document.getElementById("app"));
         event.preventDefault();
     }
 
